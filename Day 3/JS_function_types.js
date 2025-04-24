ed46b4ac46685234c3869d49729d0c4f27bd9487
Uttam Shreyakar <39872039+uttamds@users.noreply.github.com>
2025-04-24 23:42:59 +0530

    Create JS_function_types.js

---
+++ Day 3/JS_function_types.js
@@ -0,0 +1,27 @@
+Sure! Here are just the code snippets:
+
+```javascript
+// Conventional Function
+function greet(name) {
+    return "Hello, " + name;
+}
+
+let message1 = greet("Alice");
+console.log(message1);
+```
+
+```javascript
+// Arrow Function
+const greetArrow = (name) => {
+    return "Hello, " + name;
+}
+
+let message2 = greetArrow("Bob");
+console.log(message2);
+```
+
+```javascript
+// Short Arrow Function
+const greetShort = name => "Hi, " + name;
+console.log(greetShort("Charlie"));
+```
